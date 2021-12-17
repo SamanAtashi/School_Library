47339fcffe1a31cec03df18e3ffffeb728e4f4e5
SamanAtashi <milady00126@gmail.com>
2021-12-17 18:50:21 +0300

    Add show_menu

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,4 @@
-require_relative 'corrector.rb'
+require_relative 'corrector'
 # require_relative 'person.rb'
 # require_relative 'student.rb'
 # require_relative 'teacher.rb'
@@ -7,20 +7,28 @@ require_relative 'corrector.rb'
 # require_relative 'classroom.rb'
 
 class App
+  def clear
+    system 'clear'
+  end
 
-    def clear 
-        system "clear"
-    end
+  def run
+    clear
+    puts 'Welcome to School Library App!'
+    puts
+    show_menu
+  end
 
-    def run
-        clear
-        puts "Welcome to School Library App!"
-    end    
+  def show_menu
+    options = ['List all books', 'List all people', 'Create a person', 'Create a book', 'Create a rental',
+               'List all rentals for a given person id', 'Exit']
+    options.each_with_index { |option,idx| puts "#{idx+1} - #{option}"}
+    get_option
+  end
 end
 
 def main
-    app = App.new()
-    app.run
+  app = App.new
+  app.run
 end
 
 main
