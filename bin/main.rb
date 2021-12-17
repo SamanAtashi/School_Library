44e632b8dfb20b419987638043c9b1b80b1c6bb5
SamanAtashi <milady00126@gmail.com>
2021-12-17 21:36:51 +0300

    Add listing rentals

--- bin/main.rb
+++ bin/main.rb
@@ -196,6 +196,32 @@ class App
     added_msg('Rent')
     show_menu
   end
+
+  def list_rentals_by_person_id
+    clear
+    print 'ID of person: '
+    id = gets.chomp.to_i
+
+    puts
+
+    puts 'Rentals:'
+    @rentals.each do |rental|
+      puts "  Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
+    end
+
+    puts
+
+    puts 'Type "exit" to go back to menu.'
+    loop do
+      temp = gets.chomp.downcase
+      next unless temp === 'exit'
+      puts
+
+      clear
+      show_menu
+      break
+    end
+  end
 end
 
 def main
