36a5af7c1b8eff9594be065efc6e356e513ef44c
SamanAtashi <milady00126@gmail.com>
2021-12-21 11:52:32 +0300

    DRY - combine menus def

--- bin/main.rb
+++ bin/main.rb
@@ -7,6 +7,7 @@ require './methods/choose_person'
 require './methods/list_all_people'
 require './methods/create_a_rental'
 require './methods/list_rentals_by_person'
+
 class App
   def initialize
     @list_of_books = []
@@ -26,46 +27,33 @@ class App
     clear
     puts 'Welcome to School Library App!'
     puts
-    show_menu
+    menu
   end
 
-  def show_menu
-    options = ['List all books', 'List all people', 'Create a person', 'Create a book', 'Create a rental',
-               'List all rentals for a given person id', 'Exit']
-    options.each_with_index { |option, idx| puts "#{idx + 1} - #{option}" }
-    getting_option
-  end
+  def menu
+    loop do
+      options = ['List all books', 'List all people', 'Create a person', 'Create a book', 'Create a rental',
+                 'List all rentals for a given person id', 'Exit']
+      options.each_with_index { |option, idx| puts "#{idx + 1} - #{option}" }
 
-  def getting_option
-    option = gets.chomp
-    case option
-    when '1'
-      @list_all_books_class.lists_all_books
-      show_menu
-    when '2'
-      @list_all_people_class.lists_all_people
-      show_menu
-    when '3'
-      @choose_type_of_person.choose_a_person
-      show_menu
-    when '4'
-      @create_book_class.creates_a_book
-      show_menu
-    when '5'
-      @create_new_rental.create_a_rental
-      show_menu
-    when '6'
-      @list_all_rentals.list_rentals_by_person_id
-      show_menu
-    when '7'
-      clear
-      puts 'Thank you for using this app!😀'
-      puts
-    else
-      clear
-      puts '⚠️  Please enter a number between 1 and 7'
-      puts
-      show_menu
+      option = gets.chomp
+      case option
+      when '1' then @list_all_books_class.lists_all_books
+      when '2' then @list_all_people_class.lists_all_people
+      when '3' then @choose_type_of_person.choose_a_person
+      when '4' then @create_book_class.creates_a_book
+      when '5' then @create_new_rental.create_a_rental
+      when '6' then @list_all_rentals.list_rentals_by_person_id
+      when '7'
+        clear
+        puts 'Thank you for using this app!😀'
+        puts
+        break
+      else
+        clear
+        puts '⚠️  Please enter a number between 1 and 7'
+        puts
+      end
     end
   end
 end
