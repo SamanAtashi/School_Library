0508e5df6fc6dbab3c6399ed32be82963d54a236
SamanAtashi <milady00126@gmail.com>
2021-12-23 18:49:40 +0300

    Add read_rentals

--- bin/utilities/read_people.rb
+++ bin/utilities/read_people.rb
@@ -3,31 +3,31 @@ require './classes/student'
 require './classes/teacher'
 
 class ReadPeopleClass
-    def initialize(people)
-        @people = people
+  def initialize(people)
+    @people = people
+  end
+
+  def retrieves_people
+    if File.exist?('./data/students.json')
+      file = File.read('./data/students.json')
+      students = JSON.parse(file)
+
+      students.each do |student|
+        create_student_class = Student.new(student[0], student[1], student[2])
+        @people << create_student_class
+      end
+      clear
     end
 
-    def retrieves_people
-        if File.exist?('./data/students.json')
-        file = File.read('./data/students.json')
-        students = JSON.parse(file)
+    if File.exist?('./data/teachers.json')
+      file = File.read('./data/teachers.json')
+      teachers = JSON.parse(file)
 
-        students.each do |student|
-            create_student_class = Student.new(student[0],student[1],student[2])
-            @people << create_student_class
-        end
-        clear
-        end
-
-        if File.exist?('./data/teachers.json')
-        file = File.read('./data/teachers.json')
-        teachers = JSON.parse(file)
-
-        teachers.each do |teacher|
-            create_teacher_class = Teacher.new(teacher[0],teacher[1],teacher[2])
-            @people << create_teacher_class
-        end
-        clear
-        end
+      teachers.each do |teacher|
+        create_teacher_class = Teacher.new(teacher[0], teacher[1], teacher[2])
+        @people << create_teacher_class
+      end
+      clear
     end
+  end
 end
--- bin/utilities/reading_data.rb
+++ bin/utilities/reading_data.rb
@@ -1,11 +1,13 @@
 require 'json'
 require './utilities/read_books'
 require './utilities/read_people'
+require './utilities/read_rentals'
 
 class ReadingDataClass
-  def initialize(books, people)
+  def initialize(books, people, rentals)
     @books = books
     @people = people
+    @rentals = rentals
   end
 
   def run
@@ -14,5 +16,8 @@ class ReadingDataClass
 
     read_people_class = ReadPeopleClass.new(@people)
     read_people_class.retrieves_people
+
+    read_rentals_class = ReadRentalsClass.new(@rentals, @people, @books)
+    read_rentals_class.retrieves_rentals
   end
 end
