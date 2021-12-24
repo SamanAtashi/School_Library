7c2b4db18f9f2be23990184545ae303a46f43b81
SamanAtashi <milady00126@gmail.com>
2021-12-24 12:01:12 +0300

    Fix linters

--- bin/tests/spec/rental_spec.rb
+++ bin/tests/spec/rental_spec.rb
@@ -3,24 +3,23 @@ require_relative '../../classes/person'
 require_relative '../../classes/rentals'
 
 describe Rental do
-    subject(:person) {Person.new(123,"saman",parent_permission:false)}
+  subject(:person) { Person.new(123, 'saman', parent_permission: false) }
 
-    subject(:book) {ActualBook.new("title_of_book","author_of_book")}
+  subject(:book) { ActualBook.new('title_of_book', 'author_of_book') }
 
-    subject(:rentals) {Rental.new("today",book,person)}
+  subject(:rentals) { Rental.new('today', book, person) }
 
-  it "It is the same class type as rentals class" do
+  it 'It is the same class type as rentals class' do
     expect(rentals).to be_a Rental
   end
 
-    it "should have a date" do
-        expect(rentals.date).to eql("today")
-    end
-    it "should have a book" do
-        expect(rentals.book).to eql(book)
-    end
-    it "should have a person" do
-       expect(rentals.person).to eql(person) 
-    end
-
+  it 'should have a date' do
+    expect(rentals.date).to eql('today')
+  end
+  it 'should have a book' do
+    expect(rentals.book).to eql(book)
+  end
+  it 'should have a person' do
+    expect(rentals.person).to eql(person)
+  end
 end
